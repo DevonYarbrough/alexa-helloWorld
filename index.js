9892ba6926d0775802f7dadeb8af6a5ec2d40d20
yarbde6 <devon.yarbrough@verizon.com>
2017-05-07 14:17:52 -0400

    adding blind info

--- index.js
+++ index.js
@@ -6,20 +6,37 @@ var hello = new alexa.app('hello');
 
 // Default intent
 hello.launch((req, res) => {
-	res.say('Ask me to say hi!');
+	res.say('You can ask me for the lunch menu or to lower the blinds');
 
 	// keep session open
 	// continue to listen for interacton, leave stream open
-	res.shouldEndSession(true);
+	res.shouldEndSession(false);
 });
 
-var intentOpts = {
+var lunch = {
 	"slots": {},
-	"utterances": [ "{to |} {say|speak|tell me} {hi|hello|howdy|hithere|hiya|hiya|hey|hay|heya}" ]
+	"utterances": [ "{lunch menu}" ]
 };
 
+var blind = {
+	"slots": {},
+	"utterances": [ "{lower the blinds}" ]
+};
+
+var quit = {
+	"slots": {},
+	"utterances": [ "{Thank you}" ]
+};
+
+
+
 // Create an intent called hello
-hello.intent('hello', intentOpts, (req, res) => res.say('WiTNY!').shouldEndSession(false));
+hello.intent('lunch', lunch, (req, res) => res.say('For lunch there will be turkey sandwiches and chocolate chip cookies.').shouldEndSession(false));
+
+hello.intent('blind', blind, (req, res) => res.say('I am lowering the blinds.').shouldEndSession(false));
+
+hello.intent('quit', quit, (req, res) => res.say('You are welcome.').shouldEndSession(true));
+
 
 // connect the alexa-app to AWS Lambda
 exports.handler = hello.lambda();
